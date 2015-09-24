67f7daf5c023115d7e4521d4fea36530152c551b
JJ Brinkmann <jakebrinkmann@gmail.com>
2015-09-24 19:08:07 -0400

    Also backup files on change

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 function symlink_force(){
     for file in `ls -A ${PWD}/dots`; do
-        if [[ -d ~/${file} ]] ; then
+        if [[ -a ~/${file} ]] ; then
             if [[ ! -L ~/${file} ]]; then
                 echo "Moving ~/${file} to ~/${file}.backup"
                 mv -f ~/${file} ~/${file}.backup
