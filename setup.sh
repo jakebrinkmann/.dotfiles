cd42cfa82089496fdde63eebc5593649ae36f7b6
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-01-29 01:37:31 -0600

    DEV: move vim setup to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+PREFIX=${PREFIX:-${HOME}}
+
 function symlink_force(){
     for file in `ls -A ${PWD}/dots`; do
-        if [[ -a ~/${file} ]] ; then
-            if [[ ! -L ~/${file} ]]; then
-                echo "Moving ~/${file} to ~/${file}.backup"
-                mv -f ~/${file} ~/${file}.backup
+        if [[ -a ${PREFIX}/${file} ]] ; then
+            if [[ ! -L ${PREFIX}/${file} ]]; then
+                echo "Moving ${PREFIX}/${file} to ${PREFIX}/${file}.backup"
+                mv -f ${PREFIX}/${file} ${PREFIX}/${file}.backup
             fi
         fi
-        ln -sf "${PWD}/dots/${file}" ~/${file}
+        ln -sf "${PWD}/dots/${file}" ${PREFIX}/${file}
     done
 }
 
 echo "Installing..."
 echo "Forcing symlink creation. Godspeed!"
 symlink_force
+echo "Installing Vundle for Vim..."
+git clone https://github.com/VundleVim/Vundle.vim.git ${PREFIX}/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 echo "Done."
