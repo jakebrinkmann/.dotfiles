a9129b8c2f6469bc71d4486afa748d8faddeda4f
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-06-17 15:55:02 -0500

    AWS/Docker (adubs/dckr) refactoring

--- setup.sh
+++ setup.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
-PREFIX=${PREFIX:-${HOME}}
-DOTFILES_ROOT=./dots
 
 function symlink_force(){
-    for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
-      do
+    # Runtime Configurations! (or, runcoms)
+    ROOT=./dots
+    PREFIX=${PREFIX:-${HOME}}
+    mkdir -p $PREFIX
+    for src in $(find -H "$ROOT" -name '*.symlink' -not -path '*.git*'); do
         dst="${PREFIX}/.$(basename "${src%.*}")"
         ln -sf $(readlink -f ${src}) ${dst}
     done
-}
 
+    # Functions!
+    ROOT=./bin
+    PREFIX=${PREFIX:-/usr/local/bin}
+    mkdir -p $PREFIX
+    for file in $(find "${ROOT}" -type f ); do
+	    f=$(basename $file);
+	    ln -sf $(readlink -f $file) ${PREFIX}/$f; 
+    done
+}
 echo "Installing..."
 echo "Forcing symlink creation. Godspeed!"
 symlink_force
+
 echo "Installing Vundle for Vim..."
+PREFIX=${PREFIX:-${HOME}}
 mkdir -p ${PREFIX}/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ${PREFIX}/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
 echo "Done."
