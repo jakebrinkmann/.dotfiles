3daa66143127d2c2f0cc6b6f247f66d0ccf7c2d9
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-06-27 11:47:31 -0500

    Add nerd-fonts

--- setup.sh
+++ setup.sh
@@ -27,4 +27,8 @@ mkdir -p ${PREFIX}/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ${PREFIX}/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
+###### TODO: Really, will want to set this as the font in the terminal #####
+# echo "Installing Nerd Fonts..."
+# mkdir -p ~/.local/share/fonts
+# cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 echo "Done."
