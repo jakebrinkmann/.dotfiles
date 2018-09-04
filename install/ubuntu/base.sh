a6f776d13056f108079218ae4c2f26faa775eade
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-09-04 12:28:59 -0500

    Fix docker-compose install

--- install/ubuntu/base.sh
+++ install/ubuntu/base.sh
@@ -2,5 +2,10 @@
 # Installs apt packages needed by other packages
 
 sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y \
+	apt-transport-https \
+	ca-certificates \
+	curl \
+	software-properties-common \
+	openssl
 
--- install/ubuntu/docker.sh
+++ install/ubuntu/docker.sh
@@ -29,9 +29,8 @@ sudo systemctl enable docker.service
 sudo usermod -aG docker ${USER}
 
 # Install docker-compose
-sudo curl -L \
-	"https://github.com/docker/compose/releases/download/\
-	${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" \
-       	-o /usr/local/bin/docker-compose
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+.*$" | tail -n 1`
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
+sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
