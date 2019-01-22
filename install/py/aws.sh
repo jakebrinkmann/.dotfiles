b93de3e8921311f1b68716292a8977580bc7174a
Jake Brinkmann <jakebrinkmann@gmail.com>
2019-01-21 18:38:32 -0600

    fix(_aws): install pip only if not found

--- install/py/aws.sh
+++ install/py/aws.sh
@@ -3,7 +3,9 @@
 # TODO: checkout github.com/awslabs/aws-shell
 
 # Install python dependency manager
-wget -O- https://bootstrap.pypa.io/get-pip.py | sudo python3
+if ! [ -x "$(command -v pip)" ]; then
+  wget -O- https://bootstrap.pypa.io/get-pip.py | sudo python3
+fi
 
 # Install required AWS CLI
 pip install --user awscli
