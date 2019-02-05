67da70c734f2a4edb4644517554400d275ffb47f
Jake Brinkmann <jakebrinkmann@gmail.com>
2019-02-04 21:53:27 -0600

    WIP: SQUASH THIS -- remove install/centos/clojure.sh

--- install/centos/clojure.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-yum install -y emacs java-1.8.0openjdk
-curl -fsSLo /usr/local/bin/boot https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh 
-chmod 755 /usr/local/bin/boot
-
