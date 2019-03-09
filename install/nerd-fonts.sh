2848c6a8e93ad9507329b150ab7751402cfadf82
Jake Brinkmann <jakebrinkmann@gmail.com>
2019-03-09 17:04:55 -0600

    chore(fonts): simplify font install script

--- install/nerd-fonts.sh
+++ install/nerd-fonts.sh
@@ -1,36 +1,31 @@
 #!/usr/bin/env bash
-# Installs Nerd Fonts, for more Glyph support
-# //github.com/ryanoasis/nerd-fonts
+# Installs several fonts, for more Glyph support
+# 
+#       Rebuild font-cache: fc-cache -f -v
+#       Find a font: fc-match -v 'DejaVu Sans'
 
-FILENAME=DejaVuSansMono.zip
-PKGVERSN=2.0.0
-FILESHA1=701b7e1f6cf9461dfc29515943fa972e423e7251
+# Check that a non-root user is caller
+[ $(/usr/bin/id -u) -eq 0 ] \
+    && { echo 'Cannot run "makepkg" as root!'; exit 1; }
 
-# Must be run as root
-[ $(/usr/bin/id -u) -ne 0 ] \
-    && echo 'Must be run as root!' \
-    && exit 1
+# Check for passwordless SUDO
+CANSUDO=""
+command -v sudo > /dev/null && CANSUDO="sudo"
 
-# Download the TrueType font (.ttf) outline files
-# (can handle multiple fonts wihtin a single file)
-cd /tmp
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v$PKGVERSN/$FILENAME
+# Use standard packages where possible
+$CANSUDO pacman -S --noconfirm \
+    ttf-freefont \
+    adobe-source-code-pro-fonts
 
-# Verify the data integrity
-[ $(shasum $FILENAME | awk '{print $1}') == "$FILESHA1" ] || \
-    { echo 'Checksum mismatch'; exit 1; }
+# Use AUR for community packages
+for pkgname in "nerd-fonts-dejavu-complete" "ttf-unifont"; do
+    ## only download source once
+    [ -d /tmp/$pkgname ] || \
+        git clone https://aur.archlinux.org/$pkgname.git /tmp/$pkgname
 
-# Extract the archive bundle
-7z x $FILENAME
-
-# Install into /usr/share/fonts
-find . -iname "*.ttf" \
-    -not -iname "*Windows Compatible.otf" \
-    -execdir install -Dm644 {} "/usr/share/fonts/TTF/{}" \;
-
-# Force a refresh of the font-cache (~/.cache)
-fc-cache -f -v
-
-# Check that the font subsitution is working
-fc-match --verbose "DejaVuSansMono Nerd Font Mono" \
-    |grep 'fullname: "DejaVu Sans Mono Nerd Font Complete Mono"(s)'
+    ## force recompile & install all
+    ## --skippgpcheck: ttf-unifont fails from unrecognized MIT pgp key...
+    cd /tmp/$pkgname
+    makepkg -Acsf --skippgpcheck \
+        && $CANSUDO pacman --noconfirm -U *.pkg.tar.xz
+done
