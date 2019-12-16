36be6cf89751d799280c09bfbc4acbaad1409c6d
Jake Brinkmann <jakebrinkmann@gmail.com>
2019-12-16 16:42:36 -0600

    install: script for aws and sam

--- install/aws.sh
+++ install/aws.sh
@@ -3,9 +3,14 @@
 # TODO: checkout github.com/awslabs/aws-shell
 
 # Install python dependency manager
-if ! [ -x "$(command -v pip)" ]; then
+if ! [ -x "$(command -v pip3)" ]; then
   wget -O- https://bootstrap.pypa.io/get-pip.py | sudo python3
 fi
 
 # Install required AWS CLI
-pip install --user awscli
+pip3 install --user \
+  awscli \
+  aws-sam-cli
+
+echo "AWS Version: $(aws --version)"
+echo "AWS SAM Version: $(sam --version)"
