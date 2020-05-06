d9e07444a58d37906ea4c871173f6f28e143874c
Jake Brinkmann <jakebrinkmann@gmail.com>
2020-05-06 05:20:32 -0500

    feat(python): install multiple ppa versions

--- install/python3.8-ppa.sh
+++ install/python3.8-ppa.sh
@@ -1,10 +1,27 @@
-# WIP
+# WIP - ONLY FOR UBUNTU (WSL)
 
-INSTALL_PYTHON="3.8"
+VERSION="3.8"
+PYTHON="python${VERSION}"
 
+# Install from PPA
 apt update
 apt install software-properties-common
 add-apt-repository ppa:deadsnakes/ppa -y
-apt install "python${INSTALL_PYTHON}" -y
+apt install -y \
+    "${PYTHON}" \
+    "${PYTHON}-venv"
 
-curl -sS https://bootstrap.pypa.io/get-pip.py | "python${INSTALL_PYTHON}"
+# Install PIP
+curl -sS https://bootstrap.pypa.io/get-pip.py | "${PYTHON}"
+${PYTHON} -m pip install --upgrade pip
+
+# Install my favorite tools
+${PYTHON} -m pip install --upgrade \
+    pytest \
+    pytest-sugar \
+    pytest-mock \
+    black \
+    poetry
+
+# Enable bash completions for poetry
+${PYTHON} -m poetry completions bash > /etc/bash_completion.d/poetry.bash-completion
