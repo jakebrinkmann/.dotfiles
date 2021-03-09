cc06981a5e5c351e1da8e61711f9f3e7cb87a019
Jake Brinkmann <jakebrinkmann@gmail.com>
2021-03-08 20:06:40 -0600

    feat(aws): upgrades for aws cli v2

--- install/aws.sh
+++ install/aws.sh
@@ -7,21 +7,36 @@ if ! [ -x "$(command -v pip3)" ]; then
   wget -O- https://bootstrap.pypa.io/get-pip.py | sudo python3
 fi
 
-# Install required AWS CLI
+# Install AWS CLI Version 2
+cd /tmp
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install -i ~/.local/lib/python3.8/site-packages/aws-cli -b ~/.local/bin
+rm awscliv2.zip
+
+echo "AWS Version: $(aws --version)"
+
+# Install AWS SAM ClI
+cd /tmp
+curl -L "https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip" -o "aws-sam-cli-linux-x86_64.zip"
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+./sam-installation/install -i ~/.local/lib/python3.8/site-packages/aws-sam-cli -b ~/.local/bin
+rm aws-sam-cli-linux-x86_64.zip
+
+echo "AWS SAM Version: $(sam --version)"
+
+# Install cfn-lint
 pip3 install --upgrade --user \
-  awscli \
   awscurl \
-  aws-sam-cli \
   cfn-lint \
   aws-cdk.core
 
+echo "CloudFormation Linter: $(cfn-lint --version)"
+
+# Install Amplify
 sudo npm install -g \
     aws-cdk \
     @aws-amplify/cli
-
-echo "AWS Version: $(aws --version)"
-echo "AWS SAM Version: $(sam --version)"
-echo "CloudFormation Linter: $(cfn-lint --version)"
 
 if [ -n "$(type yum 2>/dev/null)" ]; then       ## CentOS/Fedora ##
   curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
