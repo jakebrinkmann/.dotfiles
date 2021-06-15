cd95e10c2bbc0c824c9f5494a20e23299fc24349
Jake Brinkmann <jakebrinkmann@gmail.com>
2021-06-15 08:42:14 -0500

    feat(timew): added timewarrior because its great

--- install/task-warrior.sh
+++ install/task-warrior.sh
@@ -4,6 +4,7 @@
 # Installs:
 # * https://taskwarrior.org/docs/
 # * https://bugwarrior.readthedocs.io/en/latest/
+# * https://timewarrior.net/docs/
 
 # All must be run as root
 [ $(/usr/bin/id -u) -ne 0 ] \
@@ -11,9 +12,27 @@
     && exit 1
 
 apt-get install --assume-yes --fix-missing \
-        taskwarrior
+        taskwarrior \
+        timewarrior
 
 python3.8 -m pip install --upgrade \
   "bugwarrior[jira]"
 
 # I don't like timers, so I like to manually run `bugwarrior-pull`
+
+# * https://timewarrior.net/docs/taskwarrior/
+cp /usr/share/doc/timewarrior/ext/on-modify.timewarrior ~/.task/hooks/
+chmod +x ~/.task/hooks/on-modify.timewarrior
+
+task diagnostics
+
+######
+# task add "Be Awesome" +life
+# task :LATEST start
+# task :LATEST stop
+# task :LATEST start
+# task :LATEST done
+# timew summary yesterday - now :ids
+# timew track 9am for 2h 'Chat with coworker' +topic
+# timew gaps
+######
