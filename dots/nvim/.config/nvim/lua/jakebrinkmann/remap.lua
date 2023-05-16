b02cbc6acf084940646b1f99a250df3037d3ca84
Jake Brinkmann <jakebrinkmann@gmail.com>
2023-05-16 15:51:57 -0500

    feat(nvim): fiddle with keymaps

--- dots/nvim/.config/nvim/lua/jakebrinkmann/remap.lua
+++ dots/nvim/.config/nvim/lua/jakebrinkmann/remap.lua
@@ -1,22 +1,28 @@
 vim.g.mapleader = " "
 
 -- [[ Basic Keymaps ]]
-vim.keymap.set('n', '<C-h>', [[<Cmd>wincmd h<CR>]])
-vim.keymap.set('n', '<C-j>', [[<Cmd>wincmd j<CR>]])
-vim.keymap.set('n', '<C-k>', [[<Cmd>wincmd k<CR>]])
-vim.keymap.set('n', '<C-l>', [[<Cmd>wincmd l<CR>]])
-vim.keymap.set('n', '<leader>vs', vim.cmd.vsplit)
+vim.keymap.set("n", "<C-h>", [[<Cmd>wincmd h<CR>]])
+vim.keymap.set("n", "<C-j>", [[<Cmd>wincmd j<CR>]])
+vim.keymap.set("n", "<C-k>", [[<Cmd>wincmd k<CR>]])
+vim.keymap.set("n", "<C-l>", [[<Cmd>wincmd l<CR>]])
+vim.keymap.set("n", "<leader>vs", vim.cmd.vsplit)
+
+-- [[ \\ switches back/forth between buffers ]]
+vim.keymap.set("n", "\\\\", [[<C-^>]])
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+
+-- Remap the CMap
+vim.keymap.set("c", "ZMD", [[<C-R>=strftime("%y%m%d-%H%M-")<CR>]])
