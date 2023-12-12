9b4ac7e1709948b9bdebd3d4adae38112e36bbb7
Jake Brinkmann <jakebrinkmann@gmail.com>
2023-12-12 13:46:49 -0600

    fix(nvim): hint diagnostic sign

--- dots/nvim/.config/nvim/init.lua
+++ dots/nvim/.config/nvim/init.lua
@@ -33,10 +33,10 @@ vim.o.smartcase = true
 
 -- Keep signcolumn on by default
 vim.wo.signcolumn = "yes"
-vim.fn.sign_define("DiagnosticSignError", {text="", texthl="DiagnosticError"})
-vim.fn.sign_define("DiagnosticSignWarn", {text="", texthl="DiagnosticWarn"})
-vim.fn.sign_define("DiagnosticSignInfo", {text="", texthl="DiagnosticInfo"})
-vim.fn.sign_define("DiagnosticSignHint", {text="", texthl="DiagnosticHint"})
+vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "§", texthl = "DiagnosticHint" })
 
 -- Decrease update time
 vim.o.updatetime = 1000
