51fc415b44385a14f77f65cb817db80f2a9b6de8
Jake Brinkmann <jakebrinkmann@gmail.com>
2024-03-15 13:50:52 -0500

    feat(gh): download all my stuff

--- download_stars.sh
+++ download_stars.sh
@@ -8,3 +8,18 @@ for x in $(gh api user/starred | jq -r '.[].full_name'); do
     git clone --filter=blob:none git@github.com:$x.git $SRC/$x
   fi
 done
+
+for x in $(gh api user/repos | jq -r '.[].full_name'); do
+  if [ ! -d $SRC/$x ]; then
+    # NOTE: using 'blobless clone' to make it fast.
+    git clone --filter=blob:none git@github.com:$x.git $SRC/$x
+  fi
+done
+
+USER=$(gh api user | jq -r '.login')
+for x in $(gh api gists | jq -r '.[].id'); do
+  if [ ! -d $SRC/$USER/$x ]; then
+    # NOTE: using 'blobless clone' to make it fast.
+    git clone --filter=blob:none git@github.com:$x.git $SRC/$USER/$x
+  fi
+done
