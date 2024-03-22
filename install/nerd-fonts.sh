d69144b05a9d0ef71dee519bfd3f3df6b209a83c
Jake Brinkmann <jakebrinkmann@gmail.com>
2024-03-22 09:17:32 -0500

    fix(install): use official repos for nerd-fonts

--- install/nerd-fonts.sh
+++ install/nerd-fonts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Installs several fonts, for more Glyph support
-# 
+#
 #       Rebuild font-cache: fc-cache -f -v
 #       Find a font: fc-match -v 'DejaVu Sans'
 
@@ -15,17 +15,19 @@ command -v sudo > /dev/null && CANSUDO="sudo"
 # Use standard packages where possible
 $CANSUDO pacman -S --noconfirm \
     ttf-freefont \
-    adobe-source-code-pro-fonts
+    adobe-source-code-pro-fonts \
+    ttf-nerd-fonts-symbols-mono \
+    nerd-fonts
 
-# Use AUR for community packages
-for pkgname in "nerd-fonts-dejavu-complete" "ttf-unifont"; do
-    ## only download source once
-    [ -d /tmp/$pkgname ] || \
-        git clone https://aur.archlinux.org/$pkgname.git /tmp/$pkgname
-
-    ## force recompile & install all
-    ## --skippgpcheck: ttf-unifont fails from unrecognized MIT pgp key...
-    cd /tmp/$pkgname
-    makepkg -Acsf --skippgpcheck \
-        && $CANSUDO pacman --noconfirm -U *.pkg.tar.xz
-done
+# # Use AUR for community packages
+# for pkgname in "nerd-fonts-dejavu-complete" "ttf-unifont"; do
+#     ## only download source once
+#     [ -d /tmp/$pkgname ] || \
+#         git clone https://aur.archlinux.org/$pkgname.git /tmp/$pkgname
+#
+#     ## force recompile & install all
+#     ## --skippgpcheck: ttf-unifont fails from unrecognized MIT pgp key...
+#     cd /tmp/$pkgname
+#     makepkg -Acsf --skippgpcheck \
+#         && $CANSUDO pacman --noconfirm -U *.pkg.tar.xz
+# done
