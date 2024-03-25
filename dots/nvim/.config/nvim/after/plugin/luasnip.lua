9719cd2c8b2524754c502021a265d30f7b0ccf09
Jake Brinkmann <jakebrinkmann@gmail.com>
2024-03-25 12:04:25 -0500

    chore(nvim): migrate snippets here

--- dots/nvim/.config/nvim/after/plugin/luasnip.lua
+++ dots/nvim/.config/nvim/after/plugin/luasnip.lua
@@ -16,7 +16,7 @@ ls.setup({
 		html = { "javascript" },
 	}),
 })
-require("luasnip.loaders.from_snipmate").lazy_load({ paths = "~/.snippets" })
+require("luasnip.loaders.from_snipmate").lazy_load({ paths = "~/.config/nvim/snippets" })
 
 vim.keymap.set({ "i" }, "<C-K>", function()
 	ls.expand()
---
+++ dots/nvim/.config/nvim/snippets/README.md
@@ -0,0 +1,1 @@
+git clone git@github.com:jakebrinkmann/snippets.git ~/.snippets
