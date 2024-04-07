e86d4d525051c854a07d11596b548352c5cbecf7
Jake Brinkmann <jakebrinkmann@gmail.com>
2024-04-07 17:05:33 -0500

    fix(sh): correct dependency order of dotfiles easy installer

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -7,14 +7,14 @@ stow -t ~ bash git nvim brew ripgrep python psql zsh bin ssh task
 stow -t ~/.config/ vscode
 [[ "$OSTYPE" == "darwin"* ]] && stow -t ~/Library/Application\ Support vscode
 
-# https://ohmyz.sh/
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # https://brew.sh/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 cd "$HOME" || exit
 brew bundle install
+
+# https://ohmyz.sh/
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Change default shell to zsh
 chsh -s /bin/zsh
