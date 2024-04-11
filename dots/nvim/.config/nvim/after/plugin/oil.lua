4cb9e16810413b2d9c5224e5610299b5db56b22e
Jake Brinkmann <jakebrinkmann@gmail.com>
2024-04-11 08:09:10 -0500

    feat(nvim): show hidden files in oil

--- dots/nvim/.config/nvim/after/plugin/oil.lua
+++ dots/nvim/.config/nvim/after/plugin/oil.lua
@@ -1,1 +1,5 @@
-vim.keymap.set("n", "<Leader>--", require("oil").open, { desc = "Open current directory" })
+local oil = require("oil")
+oil.setup({
+	view_options = { show_hidden = true },
+})
+vim.keymap.set("n", "<Leader>--", oil.open, { desc = "Open current directory" })
