14f072bf33dc1ad6921a1b3872705c96c804f4ac
Jake Brinkmann <jakebrinkmann@gmail.com>
2024-04-18 14:50:43 -0500

    feat(nvim): set expandtab always

--- dots/nvim/.config/nvim/init.lua
+++ dots/nvim/.config/nvim/init.lua
@@ -4,59 +4,12 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- [[ Setting options ]]
--- See `:help vim.o`
-
--- Set highlight on search
-vim.o.hlsearch = true
-
--- Make line numbers default
-vim.wo.number = true
-
--- Disable mouse mode
-vim.o.mouse = nil
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = "unnamedplus"
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = "yes"
-vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "§", texthl = "DiagnosticHint" })
-
--- Decrease update time
-vim.o.updatetime = 1000
-vim.o.timeout = true
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = "menuone,noselect"
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
-
--- Set vim title
-vim.o.title = true
-vim.o.titlestring = [[%f %h%m%r%w]]
-
-require("config.remap")
-require("config.plugins")
 require("config.autocmds")
+require("config.options")
+require("config.plugins")
+require("config.remap")
 require("config.usercmds")
+require("config.window")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
