6365130cfc278ae7b41c8b2e4aba89b1f960f09c
Jake Brinkmann <jakebrinkmann@gmail.com>
2024-05-31 16:18:34 -0500

    feat(nvim): continue playing with LazyVim

--- dots/nvim/.config/nvim/lua/config/options.lua
+++ dots/nvim/.config/nvim/lua/config/options.lua
@@ -2,4 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.winbar = "%=%m %f"
+-- vim.opt.winbar = "%=%m %f" -- content at top right of every buffer (useful for splits)
