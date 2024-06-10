f0e3e161f7732b3bab0594be42c29713052972ba
Jake Brinkmann <jakebrinkmann@gmail.com>
2024-06-10 12:34:09 -0500

    feat(nvim): add titles to windows

--- dots/nvim/.config/nvim/lua/config/options.lua
+++ dots/nvim/.config/nvim/lua/config/options.lua
@@ -1,5 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local o = vim.opt
+
+-- set window title
+o.title = true
+o.titlestring = [[%f %h%m%r%w]]
 
 -- vim.opt.winbar = "%=%m %f" -- content at top right of every buffer (useful for splits)
