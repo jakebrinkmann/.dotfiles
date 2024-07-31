516cb083f2fc805d012600e390d3478a0b8b8beb
Jake Brinkmann <jakebrinkmann@gmail.com>
2024-07-31 11:43:47 -0500

    feat(nvim.autocmds): add trim trailing whitespace

--- dots/nvim/.config/nvim/lua/config/autocmds.lua
+++ dots/nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,23 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- remove trailing whitespace on save
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("trim_whitespaces", { clear = true }),
+  desc = "Trim trailing white spaces",
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "<buffer>",
+      -- Trim trailing whitespaces
+      callback = function()
+        -- Save cursor position to restore later
+        local curpos = vim.api.nvim_win_get_cursor(0)
+        -- Search and replace trailing whitespaces
+        vim.cmd([[keeppatterns %s/\s\+$//e]])
+        vim.api.nvim_win_set_cursor(0, curpos)
+      end,
+    })
+  end,
+})
