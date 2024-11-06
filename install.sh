a713a852d926569a1357861177dc2c7b836677f7
Jake <jakebrinkmann@gmail.com>
2024-11-06 09:40:04 -0600

    fix(install): add base development tools

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 set -eoux
 
 # https://brew.sh/
-NONINTERACTIVE=1 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && \
-    brew install stow
+NONINTERACTIVE=1 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" &&
+  brew install stow
 
 # https://ohmyz.sh/
-[ ! -e ~/.oh-my-zsh ] && \
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended && \
-    git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/.oh-my-zsh/custom/plugins/you-should-use && \
-    [ -f ~/.zshrc ] && mv ~/.zshrc{,.bak}
+[ ! -e ~/.oh-my-zsh ] &&
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
+  git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/.oh-my-zsh/custom/plugins/you-should-use &&
+  [ -f ~/.zshrc ] && mv ~/.zshrc{,.bak}
 
-[ ! -e ~/.dotfiles ] && \
-    git clone git@github.com:jakebrinkmann/.dotfiles.git ~/.dotfiles 
+[ ! -e ~/.dotfiles ] &&
+  git clone git@github.com:jakebrinkmann/.dotfiles.git ~/.dotfiles
 
 cd "$HOME/.dotfiles/dots" || exit
 stow --adopt -t ~ bash git nvim brew ripgrep python psql zsh bin ssh task jq
@@ -22,3 +22,11 @@ stow --adopt -t ~/.config/ vscode
 cd "$HOME" || exit
 brew install --cask font-hack-nerd-font
 brew bundle --global
+
+if grep -qE '^ID=(arch|manjaro)$' /etc/os-release; then
+  sudo pacman -S base-devel
+else
+  xcode-select --install
+fi
+
+bob use stable # install nvim
