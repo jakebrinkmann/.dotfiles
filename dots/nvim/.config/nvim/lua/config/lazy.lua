14d8e5b1f5afae8cf3b688b6d5468a148bfac343
Jake Brinkmann <jakebrinkmann@gmail.com>
2024-11-07 15:41:42 -0600

    feat(nvim): add extras for markdown, prettier, [vue]

--- dots/nvim/.config/nvim/lua/config/lazy.lua
+++ dots/nvim/.config/nvim/lua/config/lazy.lua
@@ -15,9 +15,12 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.vue" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
