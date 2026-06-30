cc2f4654f0a2d77d9d89ba8fcfb6621c94b1b511
Jake Brinkmann <jakebrinkmann@gmail.com>
2026-06-29 19:25:46 -0500

    fix(collab_partner): context window

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ brew install stow
   git clone git@github.com:jakebrinkmann/.dotfiles.git ~/.dotfiles
 
 cd "$HOME/.dotfiles/dots" || exit
-stow --adopt -t ~ bash git nvim brew ripgrep python psql zsh bin ssh task jq ghostty act copilot ollama
+stow --adopt -t ~ bash git nvim brew ripgrep python psql zsh bin ssh task jq ghostty act copilot ollama opencode
 stow --adopt -t ~/.config/ vscode
 [[ "$OSTYPE" == "darwin"* ]] &&
   stow -t ~/Library/Application\ Support vscode &&
